1a0ccce3d56c6c77e8def95380c85ed29313ffc5
Sean Guerrero <seanguerrero549@gmail.com>
2023-06-16 14:10:17 -0700

    test

--- public/main.js
+++ public/main.js
@@ -1,42 +1,42 @@
-const myNodeList = document.getElementsByTagName('tr')
-for (let i = 0; i < myNodeList.length; i++) {
-    let span = document.createElement('span');
-    let txt = document.createTextNode('\u00D7')
-    span.className = "close";
-    span.appendChild(txt);
-    myNodeList[i].appendChild(span);
-}
+// const myNodeList = document.getElementsByTagName('tr')
+// for (let i = 0; i < myNodeList.length; i++) {
+//     let span = document.createElement('span');
+//     let txt = document.createTextNode('\u00D7')
+//     span.className = "close";
+//     span.appendChild(txt);
+//     myNodeList[i].appendChild(span);
+// }
 
-const close = document.getElementsByClassName('close');
-for(let i = 0; i < close.length; i++) {
-    close[i].onclick = () => {
-        let div = this.parentElement;
-        div.style.display = "none";
-    }
-}
+// const close = document.getElementsByClassName('close');
+// for(let i = 0; i < close.length; i++) {
+//     close[i].onclick = () => {
+//         let div = this.parentElement;
+//         div.style.display = "none";
+//     }
+// }
 const body = document.getElementsByTagName('body')
 
-// async function getAll () {
-//     try {
-//         const response = await fetch("https://movies-db-team3.onrender.com/movies_to_watch");
-//         console.log(response)
-//         const data = await response.json();
-//         console.log(data);
-//         let temp = "";
-//         for (i = 0; i < data.length; i++) {
-//             temp+="<tr>";
-//             temp+=`<td>${data[i].title}</td>`
-//             temp+=`<td>${data[i].genre}</td>`
-//             temp+=`<td>${data[i].rating}</td>`
-//         }
-//         document.getElementById('data').innerHTML=temp;
-//     }
-//     catch(err) {
-//         console.error(err.message)
-//     }
+async function getAll () {
+    try {
+        const response = await fetch("https://movies-db-team3.onrender.com/movies_to_watch");
+        console.log(response)
+        const data = await response.json();
+        console.log(data);
+        let temp = "";
+        for (i = 0; i < data.length; i++) {
+            temp+="<tr>";
+            temp+=`<td>${data[i].title}</td>`
+            temp+=`<td>${data[i].genre}</td>`
+            temp+=`<td>${data[i].rating}</td>`
+        }
+        document.getElementById('data').innerHTML=temp;
+    }
+    catch(err) {
+        console.error(err.message)
+    }
 
-// }
-// getAll();
+}
+getAll();
 
 ////////////////////POST REQUEST/////////////////
 document.getElementById('form').addEventListener('submit', function(event) {
